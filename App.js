c578f6631159738c4ae28a3f5d45a45ff617e738
HINA KHADIM <hinakhadim2002@gmail.com>
2020-06-24 23:38:01 +0500

    image view and button

--- App.js
+++ App.js
@@ -1,19 +1,43 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  TouchableWithoutFeedback,
+  View,
+  Image,
+  SafeAreaView,
+  Button,
+} from "react-native";
 
 export default function App() {
+  const handlePress = () => console.log("tExt pressed");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} onPress={handlePress}>
+        Hello World{" "}
+      </Text>
+      <Button title="Click Me"></Button>
+      <TouchableWithoutFeedback>
+        <Image
+          blurRadius={2}
+          fadeDuration={1000}
+          source={{
+            width: 200,
+            height: 300,
+            uri:
+              "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg",
+          }}
+        />
+      </TouchableWithoutFeedback>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
