04f882438a8a27d588bf188e7c53b843c0389db9
HINA KHADIM <hinakhadim2002@gmail.com>
2020-06-25 22:43:03 +0500

    layouts and dimensions

--- App.js
+++ App.js
@@ -7,16 +7,30 @@ import {
   Image,
   SafeAreaView,
   Button,
+  Alert,
+  StatusBar,
+  Platform,
+  Dimensions,
 } from "react-native";
 
 export default function App() {
   const handlePress = () => console.log("tExt pressed");
+
+  console.log(Dimensions.get("screen"));
   return (
     <SafeAreaView style={styles.container}>
       <Text numberOfLines={1} onPress={handlePress}>
-        Hello World{" "}
+        Hello World
       </Text>
-      <Button title="Click Me"></Button>
+      <Button
+        title="Click Me"
+        onPress={() =>
+          Alert.alert("My title", "My message", [
+            { text: "Yes", onPress: () => console.log("yes") },
+            { text: "No", onPress: () => console.log("No") },
+          ])
+        }
+      ></Button>
       <TouchableWithoutFeedback>
         <Image
           blurRadius={2}
@@ -29,6 +43,12 @@ export default function App() {
           }}
         />
       </TouchableWithoutFeedback>
+      <Button
+        title="Prompt"
+        onPress={() =>
+          Alert.prompt("My title ", "My msg", (text) => console.log(text))
+        }
+      ></Button>
     </SafeAreaView>
   );
 }
@@ -39,5 +59,6 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
 });
